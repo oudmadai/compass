f51f1a35c0270cee9ae825440f90b60a08a63e22
oudmadai <me3348637@gmail.com>
2025-04-23 10:07:39 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// build.gradle.kts (Project level)
+plugins {
+    kotlin("android") version "1.9.22" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
